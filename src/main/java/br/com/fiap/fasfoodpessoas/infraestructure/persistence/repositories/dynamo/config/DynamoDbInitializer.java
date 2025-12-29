2d74e7d78d2c886404629c514c7225525c036de2
thiagolinoz <thiagolinoz@gmail.com>
2025-12-29 16:04:27 -0300

    remove profile

--- src/main/java/br/com/fiap/fasfoodpessoas/infraestructure/persistence/repositories/dynamo/config/DynamoDbInitializer.java
+++ src/main/java/br/com/fiap/fasfoodpessoas/infraestructure/persistence/repositories/dynamo/config/DynamoDbInitializer.java
@@ -4,14 +4,12 @@ import br.com.fiap.fasfoodpessoas.infraestructure.persistence.entities.PessoaEnt
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 @Configuration
-@Profile("local")
 public class DynamoDbInitializer {
 
     @Value("${aws.dynamodb.create-tables:false}")
